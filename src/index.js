b62b14c9b09ffc3559d655abaa20c44c28b3f838
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-05-04 16:49:00 -0400

    added a comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+// Hey look at me.
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
